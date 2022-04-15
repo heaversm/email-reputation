0450ae421733473a8e19704db4d7c06a681d5472
Mike Heavers <mheavers@gmail.com>
2022-04-15 15:10:44 -0700

    fix package.json

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const path = require('path');
-const express = require('express');
+const express = require("express");
 
-...
+const PORT = process.env.PORT || 3001;
 
-// Have Node serve the files for our built React app
+const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Connected to Server" });
 });
 
-// All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
